961fb8eb6b399cf2347e4fbd7f4f0b904c0bf10a
Eagleworrior <lomalianorman@gmail.com>
2026-05-26 23:02:06 +0300

    Fix formatting of PAYBILL_NUMBER environment variable

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 # backend/config.py
 import os
+
 class Config:
     PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
     PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY')
-    PAYBILL_NUMBER = os.getenv('PAYBILL_NUMBER','880100')
+    PAYBILL_NUMBER = os.getenv('PAYBILL_NUMBER', '880100')
