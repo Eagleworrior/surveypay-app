e7f0942f4e183dd504754d85cb3da5f4bcb84ca9
Eagleworrior <lomalianorman@gmail.com>
2026-05-27 00:15:04 +0300

    Remove configuration loading from config.py

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv() # Loads variables from .env file
-
-class Config:
-    PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
-    PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY')
-    PAYBILL_NUMBER = os.getenv('PAYBILL_NUMBER', '880100')
